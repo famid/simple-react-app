4df4cefe66aadc351cfb322b5f44429c5e2e9401
Ahsanul Hoque Famid <ahsanulhoque721@gmail.com>
2024-02-13 17:05:26 +0600

    Update: Separate the components for reusability

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BlogList } from './BlogList';
+import useFetch from '../useFetch';
 
 export default function Home() {
+
+  // const { blogs, isPending, error } = useFetch('http://localhost:3000/blogs');
+  const { error, isPending, data: blogs } = useFetch('http://localhost:3000/blogss');
   return (
-    <div>
-        CONTENT
+    <div className='home'>
+       { error && <div>{ error }</div> }
+        <p>{isPending && 'Loading....'}</p>
+        
+        {blogs && <BlogList blogs={blogs}/>}
     </div>
   )
 }
---
+++ src/useFetch.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      setTimeout(() => {
+
+        fetch(url)
+        .then(res => {
+          if(!res.ok) {
+            throw Error('Data could not fetched!!');
+          }
+          return res.json();
+        })
+        .then(data => {
+          setIsPending(false);
+          setData(data);
+          setError(null);
+        })
+        .catch(error => {
+          console.log(error.message);
+          setIsPending(false);
+          setError(error.message);
+          setData(null);
+        });
+
+      }, 1000);
+
+    }, [url]);
+  
+  
+    return { data, isPending, error };
+}
+
+export default useFetch;
+
+// import { useState, useEffect } from 'react';
+
+// const useFetch = (url) => {
+//   const [data, setData] = useState(null);
+//   const [isPending, setIsPending] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     setTimeout(() => {
+//       fetch(url)
+//       .then(res => {
+//         if (!res.ok) { // error coming back from server
+//           throw Error('could not fetch the data for that resource');
+//         } 
+//         return res.json();
+//       })
+//       .then(data => {
+//         setIsPending(false);
+//         setData(data);
+//         setError(null);
+//       })
+//       .catch(err => {
+//         // auto catches network / connection error
+//         setIsPending(false);
+//         setError(err.message);
+//       })
+//     }, 1000);
+//   }, [url])
+
+//   return { data, isPending, error };
+// }
+ 
+// export default useFetch;
